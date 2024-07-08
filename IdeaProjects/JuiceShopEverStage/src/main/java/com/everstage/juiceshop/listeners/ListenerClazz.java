cc466812792c8fc380d2c5fa7c39cf0fad198b52
Sundarraj R <ragusundar274@gmail.com>
2024-07-08 21:01:55 +0530

    Optimized imports

---
+++ IdeaProjects/JuiceShopEverStage/src/main/java/com/everstage/juiceshop/listeners/ListenerClazz.java
@@ -0,0 +1,8 @@
+package com.everstage.juiceshop.listeners;
+
+import org.testng.ISuiteListener;
+import org.testng.ITestListener;
+
+public class ListenerClazz implements ISuiteListener , ITestListener {
+
+}
---
+++ IdeaProjects/JuiceShopEverStage/src/main/java/com/everstage/juiceshop/reports/ExtentReports.java
@@ -0,0 +1,4 @@
+package com.everstage.juiceshop.reports;
+
+public class ExtentReports {
+}
